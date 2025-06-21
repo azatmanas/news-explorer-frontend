1d018745d78c942cb66b259b70d8c0b9232a33f9
AZAT MANAS <ZTMANAS@GMAIL.COM>
2025-06-21 13:56:29 -0400

    LoginModal created

---
+++ src/components/LoginModal/LoginModal.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function LoginModal() {
+  return (
+    <header className="header">
+      <div className="header__logo"></div>
+    </header>
+  );
+}
+
+export default LoginModal;
