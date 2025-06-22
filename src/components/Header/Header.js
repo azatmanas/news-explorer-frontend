802a45e4c03d296f1399b6a0e0fb56e8c7798ead
AZAT MANAS <ZTMANAS@GMAIL.COM>
2025-06-22 14:30:22 -0400

    app.js created

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,13 @@
 import React from "react";
-
+import backgroundImage from "./assets/background.svg";
 function Header() {
   return (
     <header className="header">
-      <div className="header__logo"></div>
+      <div className="header__logo">NewsExplorer</div>
+      <div className="header__nav">
+        <button className="">Home</button>
+        <button className="">Sign in</button>
+      </div>
     </header>
   );
 }
